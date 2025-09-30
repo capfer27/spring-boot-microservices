57608d8913f1ec8a182de7fbef183cdceac160fe
capfer27 <capfer27@gmailcom>
2025-09-30 00:08:44 -0700

    create metadata for entities 'Account' and 'Customer'

---
+++ src/main/java/com/promath/entities/Account.java
@@ -0,0 +1,28 @@
+package com.promath.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "accounts")
+public class Account extends BaseEntity {
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
---
+++ src/main/java/com/promath/entities/Customer.java
@@ -0,0 +1,29 @@
+package com.promath.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+}
