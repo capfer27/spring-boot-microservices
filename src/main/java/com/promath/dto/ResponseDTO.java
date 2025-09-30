4be3715301377afd5364abecc28aa0f94c94c193
capfer27 <capfer27@gmailcom>
2025-09-30 02:04:21 -0700

    create Data Transfer Objects 'DTOs'

---
+++ src/main/java/com/promath/dto/ResponseDTO.java
@@ -0,0 +1,12 @@
+package com.promath.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDTO {
+
+    private String statusCode;
+    private String statusMessage;
+}
