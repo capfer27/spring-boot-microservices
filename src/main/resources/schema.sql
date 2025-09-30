2759f7fcade73704dbe9ef99b2e7dfd01451ecb1
capfer27 <capfer27@gmailcom>
2025-09-29 22:59:45 -0700

    configure h2 database and add sql scripts for 'accounts' and 'customers' tables creation

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    mobile_number VARCHAR(20) NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+    account_number SERIAL PRIMARY KEY NOT NULL,
+    customer_id INT NOT NULL,
+    account_type VARCHAR(100) NOT NULL,
+    branch_address VARCHAR(200) NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR(20) DEFAULT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
