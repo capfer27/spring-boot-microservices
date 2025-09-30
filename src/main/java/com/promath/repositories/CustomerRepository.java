463eaf0a79a23a186b71220d4cfb5aacdd60c455
capfer27 <capfer27@gmailcom>
2025-09-30 00:20:45 -0700

    create repositories for entities 'Account' and 'Customer'

---
+++ src/main/java/com/promath/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.promath.repositories;
+
+import com.promath.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
