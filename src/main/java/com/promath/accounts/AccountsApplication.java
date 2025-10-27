b2473d471a6da4bcf034484266d00fd5bb8fe24c
capfer27 <capfer27@gmailcom>
2025-10-26 19:41:07 -0700

    documenting the api

--- src/main/java/com/promath/accounts/AccountsApplication.java
+++ src/main/java/com/promath/accounts/AccountsApplication.java
@@ -1,11 +1,37 @@
 package com.promath.accounts;
 
+import com.sun.tools.jconsole.JConsoleContext;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "REST API documentation for Accounts microservice",
+                description = "Capfer Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Карлуш Перейра Фернандеш",
+                        email = "capfer27@gmail.com",
+                        url = "https://ulyanovsk.hh.ru/resume/ebc7d16cff03b5060d0039ed1f515351616748"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url="https://ulyanovsk.hh.ru/resume/ebc7d16cff03b5060d0039ed1f515351616748"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Capfer Accounts microservice REST API Documentation",
+                url = "hhtps://capfer-promath.com/swagger-ui.com.html"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
