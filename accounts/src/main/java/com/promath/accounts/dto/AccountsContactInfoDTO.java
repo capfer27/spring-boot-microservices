ddf1c1e75c409bf18fe9107d7e945e1a30b83507
capfer27 <capfer27@gmailcom>
2025-11-21 23:30:56 +0400

    configure actuator

--- accounts/src/main/java/com/promath/accounts/dto/AccountsContactInfoDTO.java
+++ accounts/src/main/java/com/promath/accounts/dto/AccountsContactInfoDTO.java
@@ -1,57 +1,18 @@
 package com.promath.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @ConfigurationProperties(prefix = "accounts")
-public final class AccountsContactInfoDTO {
-    private final String message;
-    private final Map<String, String> contactDetails;
-    private final List<String> onCallSupport;
+@Getter
+@Setter
+public class AccountsContactInfoDTO {
 
-    public AccountsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-        this.message = message;
-        this.contactDetails = contactDetails;
-        this.onCallSupport = onCallSupport;
-    }
-
-    public String message() {
-        return message;
-    }
-
-    public Map<String, String> contactDetails() {
-        return contactDetails;
-    }
-
-    public List<String> onCallSupport() {
-        return onCallSupport;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (AccountsContactInfoDTO) obj;
-        return Objects.equals(this.message, that.message) &&
-                Objects.equals(this.contactDetails, that.contactDetails) &&
-                Objects.equals(this.onCallSupport, that.onCallSupport);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(message, contactDetails, onCallSupport);
-    }
-
-    @Override
-    public String toString() {
-        return "AccountsContactInfoDTO[" +
-                "message=" + message + ", " +
-                "contactDetails=" + contactDetails + ", " +
-                "onCallSupport=" + onCallSupport + ']';
-    }
-
-
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
--- cards/src/main/java/com/capfer/cards/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/capfer/cards/dto/CardsContactInfoDTO.java
@@ -1,5 +1,7 @@
 package com.capfer.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,52 +9,11 @@ import java.util.Map;
 import java.util.Objects;
 
 @ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
 public final class CardsContactInfoDTO {
-    private final String message;
-    private final Map<String, String> contactDetails;
-    private final List<String> onCallSupport;
 
-    public CardsContactInfoDTO(
-            String message, Map<String, String> contactDetails, List<String> onCallSupport
-    ) {
-        this.message = message;
-        this.contactDetails = contactDetails;
-        this.onCallSupport = onCallSupport;
-    }
-
-    public String message() {
-        return message;
-    }
-
-    public Map<String, String> contactDetails() {
-        return contactDetails;
-    }
-
-    public List<String> onCallSupport() {
-        return onCallSupport;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (CardsContactInfoDTO) obj;
-        return Objects.equals(this.message, that.message) &&
-                Objects.equals(this.contactDetails, that.contactDetails) &&
-                Objects.equals(this.onCallSupport, that.onCallSupport);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(message, contactDetails, onCallSupport);
-    }
-
-    @Override
-    public String toString() {
-        return "CardsContactInfoDTO[" +
-                "message=" + message + ", " +
-                "contactDetails=" + contactDetails + ", " +
-                "onCallSupport=" + onCallSupport + ']';
-    }
-
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
