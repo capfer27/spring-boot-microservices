cbf3da466726a405bcc5990af1abe5dc8e26c3f4
capfer27 <capfer27@gmailcom>
2025-11-21 21:19:50 +0400

    make contact info dtos mutable so the fields can be changed by spring boot actuator

--- cards/src/main/java/com/capfer/cards/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/capfer/cards/dto/CardsContactInfoDTO.java
@@ -4,9 +4,55 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDTO(
-        String message, Map<String, String> contactDetails, List<String> onCallSupport
-) {
+public final class CardsContactInfoDTO {
+    private final String message;
+    private final Map<String, String> contactDetails;
+    private final List<String> onCallSupport;
+
+    public CardsContactInfoDTO(
+            String message, Map<String, String> contactDetails, List<String> onCallSupport
+    ) {
+        this.message = message;
+        this.contactDetails = contactDetails;
+        this.onCallSupport = onCallSupport;
+    }
+
+    public String message() {
+        return message;
+    }
+
+    public Map<String, String> contactDetails() {
+        return contactDetails;
+    }
+
+    public List<String> onCallSupport() {
+        return onCallSupport;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (CardsContactInfoDTO) obj;
+        return Objects.equals(this.message, that.message) &&
+                Objects.equals(this.contactDetails, that.contactDetails) &&
+                Objects.equals(this.onCallSupport, that.onCallSupport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, contactDetails, onCallSupport);
+    }
+
+    @Override
+    public String toString() {
+        return "CardsContactInfoDTO[" +
+                "message=" + message + ", " +
+                "contactDetails=" + contactDetails + ", " +
+                "onCallSupport=" + onCallSupport + ']';
+    }
+
 }
--- loans/src/main/java/com/capfer/loans/dto/LoansContactInfoDTO.java
+++ loans/src/main/java/com/capfer/loans/dto/LoansContactInfoDTO.java
@@ -4,9 +4,55 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDTO(
-        String message, Map<String, String> contactDetails, List<String> onCallSupport
-) {
+public final class LoansContactInfoDTO {
+    private final String message;
+    private final Map<String, String> contactDetails;
+    private final List<String> onCallSupport;
+
+    public LoansContactInfoDTO(
+            String message, Map<String, String> contactDetails, List<String> onCallSupport
+    ) {
+        this.message = message;
+        this.contactDetails = contactDetails;
+        this.onCallSupport = onCallSupport;
+    }
+
+    public String message() {
+        return message;
+    }
+
+    public Map<String, String> contactDetails() {
+        return contactDetails;
+    }
+
+    public List<String> onCallSupport() {
+        return onCallSupport;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (LoansContactInfoDTO) obj;
+        return Objects.equals(this.message, that.message) &&
+                Objects.equals(this.contactDetails, that.contactDetails) &&
+                Objects.equals(this.onCallSupport, that.onCallSupport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, contactDetails, onCallSupport);
+    }
+
+    @Override
+    public String toString() {
+        return "LoansContactInfoDTO[" +
+                "message=" + message + ", " +
+                "contactDetails=" + contactDetails + ", " +
+                "onCallSupport=" + onCallSupport + ']';
+    }
+
 }
