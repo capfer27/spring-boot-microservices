17338649439e47653645261f6c8161bfb66ccadc
capfer27 <capfer27@gmailcom>
2025-11-23 11:23:59 +0400

    replace h2 database with postgres db

--- cards/src/main/resources/schema.sql
+++ cards/src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS `cards` (
-  `card_id` BIGSERIAL NOT NULL,
-  `mobile_number` VARCHAR(15) UNIQUE NOT NULL,
-  `card_number` VARCHAR(100) NOT NULL,
-  `card_type` VARCHAR(100) NOT NULL,
-  `total_limit` INTEGER NOT NULL,
-  `amount_used` INTEGER NOT NULL,
-  `available_amount` INTEGER NOT NULL,
-  `created_at` DATE NOT NULL,
-  `created_by` VARCHAR(20) NOT NULL,
-  `updated_at` DATE DEFAULT NULL,
-  `updated_by` VARCHAR(20) DEFAULT NULL,
-  PRIMARY KEY (`card_id`)
+CREATE TABLE IF NOT EXISTS cards (
+  card_id BIGSERIAL NOT NULL,
+  mobile_number VARCHAR(15) UNIQUE NOT NULL,
+  card_number VARCHAR(100) NOT NULL,
+  card_type VARCHAR(100) NOT NULL,
+  total_limit INTEGER NOT NULL,
+  amount_used INTEGER NOT NULL,
+  available_amount INTEGER NOT NULL,
+  created_at DATE NOT NULL,
+  created_by VARCHAR(20) NOT NULL,
+  updated_at DATE DEFAULT NULL,
+  updated_by VARCHAR(20) DEFAULT NULL,
+  PRIMARY KEY (card_id)
 );
