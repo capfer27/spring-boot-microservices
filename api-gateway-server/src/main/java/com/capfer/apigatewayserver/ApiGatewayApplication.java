09f13e4187af29d58d206997c8ba30e7e6234f3c
capfer27 <capfer27@gmailcom>
2026-02-15 02:11:48 +0400

    set up routing for all microservices in api-gateway-server

--- api-gateway-server/src/main/java/com/capfer/apigatewayserver/ApiGatewayApplication.java
+++ api-gateway-server/src/main/java/com/capfer/apigatewayserver/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.capfer.apigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,40 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+    /**
+     * Конфигурация маршрутов для API Gateway, которая перенаправляет запросы на соответствующие микросервисы
+     * в зависимости от пути запроса. Например, запросы, начинающиеся с /capferbank/accounts/, будут перенаправляться
+     * на микросервис Accounts, а запросы, начинающиеся с /capferbank/cards/, будут перенаправляться на микросервис Cards.
+     * Фильтры используются для переписывания пути запроса, чтобы удалить префикс /capferbank/ перед отправкой запроса
+     * на соответствующий микросервис.
+     * Каждый маршрут использует балансировщик нагрузки (lb://) для распределения запросов
+     * между экземплярами микросервиса, зарегистрированными в Eureka Server.
+     *
+     *  English:
+     * Route configuration for API Gateway that redirects requests to the corresponding microservices
+     * based on the request path. For example, requests starting with /capferbank/accounts/ will be redirected
+     * to the Accounts microservice, and requests starting with /capferbank/cards/ will be redirected to the Cards microservice.
+     * Filters are used to rewrite the request path to remove the /capferbank/ prefix before sending the request to the corresponding microservice.
+     * Each route uses a load balancer (lb://) to distribute requests among instances of the microservice registered in the Eureka Server.
+     */
+    @Bean
+    public RouteLocator capferBankRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route( p -> p
+                .path("/capferbank/accounts/**")
+                .filters(filter ->
+                        filter.rewritePath("/capferbank/accounts/(?<segment>.*)", "/${segment}"))
+                .uri("lb://ACCOUNTS"))
+                .route( p -> p
+                        .path("/capferbank/cards/**")
+                        .filters(filter ->
+                                filter.rewritePath("/capferbank/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .route(p -> p
+                        .path("/capferbank/loans/**")
+                        .filters(filter ->
+                                filter.rewritePath("/capferbank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .build();
+    }
 }
